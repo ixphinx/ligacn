681cc8e866009e77b056a2206069f746fdda449f
ixphinx <48696004+ixphinx@users.noreply.github.com>
2021-02-15 16:00:28 -0300

    Update index.js

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,35 +1,9 @@
 const express = require('express');
 const router = express.Router();
-const path = require('path');
-const { unlink } = require('fs-extra');
-const cloudinary = require('cloudinary');
-const fs = require('fs-extra');
+
 
 const Data = require('../models/database');
 
-cloudinary.config({ 
-    cloud_name: 'dhzgjgjpk', 
-    api_key: '862754171147947', 
-    api_secret: '8Aq3DIe9YwsP2Bi4pXwO4fYu6aY',
-  });
-
-  router.post('/images/add', async (req, res) => {
-    const { title, description } = req.body;
-    for (let i = 0; i < req.files.length; i++) {
-        const element = req.files[i];
-        try {
-            const result = await cloudinary.v2.uploader.upload(req.files[i].path);
-            const newPhoto = new Data({title, description, imageURL: result.url, public_id: result.public_id});
-            await newPhoto.save();
-            await fs.unlink(req.files[i].path);
-        } catch (e) {
-            console.log(e)
-        }
-        
-    }
-      res.redirect('/');
-
-});
 
 router.get('/', async (req,res)=>{
     res.header('Access-Control-Allow-Origin', '*');
